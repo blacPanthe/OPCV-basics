45aaab974ea2f1538f345da21b33a1103a813d25
Atharva Bakde <66546484+blacPanthe@users.noreply.github.com>
2021-06-05 16:29:53 +0530

    Add files via upload

---
+++ Opencv_Background_Subtraction.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture('vtest.avi')
+fgbg = cv.createBackgroundSubtractorKNN()
+
+
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        break
+
+    fgmask = fgbg.apply(frame)
+
+    cv.imshow('Frame', frame)
+    cv.imshow('FG MASK Frame', fgmask)
+
+    keyboard = cv.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
